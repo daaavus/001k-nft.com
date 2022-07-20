a0127ae1d6d7c83bca982dfefc43dfb6dc1178f7
FairyTale#5571 <onopa96@gmail.com>
2022-07-20 22:01:45 +0300

    price

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -77,7 +77,7 @@ export default function Main() {
         blockchain.smartContract.methods.mint(mintAmount).send({
         from: blockchain.account,
         value: blockchain.web3.utils.toWei(
-            (215 * mintAmount).toString(),
+            (180 * mintAmount).toString(),
             "ether"
         ),
         }).once("error", (err) => {
